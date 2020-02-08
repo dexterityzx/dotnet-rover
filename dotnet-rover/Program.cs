5d2d6e9fe1c217590c325d96a735c0b23e047a35
Tim <hsinting.shih@gmail.com>
2020-02-08 14:38:20 -0500

    Add project files.

---
+++ dotnet-rover/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_rover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
