3fa238547026df5a70605ec1d0c53e93e266b8de
Tim <hsinting.shih@gmail.com>
2020-02-09 09:16:56 -0500

    Revert "Revert "add unit test for Map and RoverState""
    This reverts commit da02dcc6037a097a318d30100f972d4c1f45e73a.

---
+++ Test/Data/TestMapBoundaryData.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+
+namespace Test.Data
+{
+    internal class TestMapBoundaryData : TheoryData<int, int, int, int, bool>
+    {
+        public TestMapBoundaryData()
+        {
+            Add(5, 5, 2, 2, true);
+            Add(5, 5, 0, 0, true);
+            Add(5, 5, 5, 5, true);
+
+            Add(5, 5, 6, 2, false);
+            Add(5, 5, 2, 6, false);
+            Add(5, 5, 6, 6, false);
+
+            Add(5, 5, -1, 2, false);
+            Add(5, 5, 2, -1, false);
+        }
+    }
+}
---
+++ Test/UnitTestMap.cs
@@ -0,0 +1,26 @@
+﻿using Rover;
+using Test.Data;
+using Xunit;
+
+namespace Test
+{
+    public class UnitTestMap
+    {
+        [Theory]
+        [ClassData(typeof(TestMapBoundaryData))]
+        public void TesMapCanCheckBoundary(int maxX, int maxY, int x, int y, bool expected)
+        {
+            var map = new Map(maxX, maxY);
+            Assert.Equal(expected, map.IsInBoundary(x, y));
+        }
+
+        [Theory]
+        [ClassData(typeof(TestMapOriginData))]
+        public void TestMapHasZeroOrigin(int maxX, int maxY, int expectedX, int expectedY)
+        {
+            var map = new Map(maxX, maxY);
+            Assert.Equal(expectedX, map.maxX);
+            Assert.Equal(expectedY, map.maxY);
+        }
+    }
+}
